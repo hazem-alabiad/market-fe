2f90d8d0b3d3a526bbe2cdf7b19f7f044e1e159f
Hazem Alabiad <hazem.alabiad@hotmail.com>
2022-10-19 12:43:42 +0300

    add svg as reactcomponent type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,8 @@
-// / <reference types="vite/client" />
-// / <reference types="vite-plugin-svgr/client" />
-// / <reference types="redux-persist" />
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+/// <reference types="redux-persist" />
+
+declare module "*.svg" {
+  const content: React.FC<React.SVGProps<SVGElement>>;
+  export default content;
+}
